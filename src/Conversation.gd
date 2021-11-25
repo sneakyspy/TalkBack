0b5a5b0bb266ab403b9326791a3edfd3c1481878
Lord-Aquarius <tito@ruoso.com>
2021-11-25 15:18:01 -0500

    Json continue 2

--- src/Conversation.gd
+++ src/Conversation.gd
@@ -1,13 +1,27 @@
 extends Node
 
 func MakeLine():
-	var file = File.new
-	file.open('res//Save.json', File.READ)
-	var dict = parse_json(file.get_as_text())
-	return dict
+	var file = File.new()
+	file.open('res://Save.json', File.READ)
+	var dictionary = parse_json(file.get_as_text())
 
+var dict = {}
+
+func load_json_file(path):
+	"""Loads a JSON file from the given res path and return the loaded JSON object."""
+	var file = File.new()
+	file.open(path, file.READ)
+	var text = file.get_as_text()
+	var result_json = JSON.parse(text)
+	if result_json.error != OK:
+		print("[load_json_file] Error loading JSON file '" + str(path) + "'.")
+		print("\tError: ", result_json.error)
+		print("\tError Line: ", result_json.error_line)
+		print("\tError String: ", result_json.error_string)
+		return null
+	var obj = result_json.result
+	print(text)
+	return obj
 
 func _ready() -> void:
-	print(MakeLine() + "BlankText")
-
-
+	print(load_json_file('res//Save.json'))
